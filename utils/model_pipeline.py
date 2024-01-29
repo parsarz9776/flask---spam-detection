02b6294a8647a572bf551cdc812b6a12bf3ae374
Parsa rezaei <105837987+parsarz9776@users.noreply.github.com>
2024-01-29 10:29:39 -0500

    Update model_pipeline.py

--- utils/model_pipeline.py
+++ utils/model_pipeline.py
@@ -1,3 +1,10 @@
 # Modules in this script will be loading your ML models from the 'model' folder, using this pretrained model to do the prediction based on the input text and send the classification output
 # from your notebook, save the trained model as a .pkl pickle file and put it inside the 'model' folder. Define a function that loads this pickle file and returns it which basically acts as the object using which you can do .predict()
 # you will be importing the functions from data_preprocess file to do the cleaning/transformation and return the classification output.
+
+
+def load_model():
+    model_path = Path('models/model_pickle.pkl')  
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+    return model
