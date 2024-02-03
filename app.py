e98630338803a9f9a4a89dccad9743204e97faff
Anirudh42 <av.anirudh.96@gmail.com>
2024-02-03 11:22:05 -0500

    updated imports

--- app.py
+++ app.py
@@ -1,48 +1,29 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
 import spacy
 from nltk.stem import PorterStemmer
 from spacy.lang.en.stop_words import STOP_WORDS
 from sklearn.feature_extraction.text import CountVectorizer
+from utils.data_preprocess import preprocess, stemmer
+from utils.model_pipeline import model, vectorizer
 
 app = Flask(__name__)
 
-# Load your saved model
-with open('models/model_pickle', 'rb') as f:
-    model = pickle.load(f)
-
-# Load the vectorizer
-with open('models/vectorizer.pkl', 'rb') as f:
-    vectorizer = pickle.load(f)
 
 # Load spaCy for text processing
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load("en_core_web_sm")
 
-# Function for text preprocessing
-def preprocess(text):
-    doc = nlp(text)
-    no_stop_words = [token.text for token in doc if not token.is_stop and not token.is_punct]
-    return " ".join(no_stop_words)
-
-# Function for stemming
-def stemmer(text):
-    text = text.split()
-    words = ''
-    stemmer = PorterStemmer()
-    for i in text:
-        words += (stemmer.stem(i)) + ' '
-    return words.strip()
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
+
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
         try:
             # Get the input text from the form
-            input_text = request.form['text']
+            input_text = request.form["text"]
 
             # Preprocess and stem the input text
             processed_text = preprocess(input_text)
@@ -55,14 +36,18 @@ def predict():
             prediction = model.predict(vectorized_text)
 
             # Output the prediction
-            result = {"Text": input_text, "Prediction": "Spam" if prediction[0] == 1 else "Ham"}
-            return render_template('result.html', result=result)
+            result = {
+                "Text": input_text,
+                "Prediction": "Spam" if prediction[0] == 1 else "Ham",
+            }
+            return render_template("result.html", result=result)
 
         except Exception as e:
             return jsonify({"error": str(e)})
 
     # If it's a GET request, render the predict page
-    return render_template('predict.html')
+    return render_template("predict.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
--- utils/model_pipeline.py
+++ utils/model_pipeline.py
@@ -1,3 +1,12 @@
 # Modules in this script will be loading your ML models from the 'model' folder, using this pretrained model to do the prediction based on the input text and send the classification output
 # from your notebook, save the trained model as a .pkl pickle file and put it inside the 'model' folder. Define a function that loads this pickle file and returns it which basically acts as the object using which you can do .predict()
 # you will be importing the functions from data_preprocess file to do the cleaning/transformation and return the classification output.
+# Load your saved model
+import pickle
+
+with open("models/model_pickle", "rb") as f:
+    model = pickle.load(f)
+
+# Load the vectorizer
+with open("models/vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
